a10b21585ef258cf878cefb53695964a4cff5343
Felix Rilling <contact@f-rilling.com>
2017-11-07 17:04:29 +0100

    updated default axios settings

--- test.js
+++ test.js
@@ -16,15 +16,14 @@ const {
 /* getUsage("2017-03", "battlefactory-1760")
     .then(data => console.log("Usage data for 2017-03 battlefactory-1760:", data)); */
 
-
-getLeads("2017-03", "battlefactory-1760")
-    .then(data => console.log("Lead data for 2017-03 battlefactory-1760:", data));
+/* getLeads("2017-03", "battlefactory-1760")
+    .then(data => console.log("Lead data for 2017-03 battlefactory-1760:", data)); */
 
 // Example: load latest OU data
-/* getTimeframes()
+getTimeframes()
     .then(timeframes => {
         const latest = timeframes[timeframes.length - 1];
 
         getUsage(latest, "gen7ou-0")
             .then(data => console.log(data.stats));
-    }); */
+    });
