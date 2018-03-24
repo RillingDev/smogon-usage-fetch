e4097b3933ccc09609e4c5532fe44272de5bedfa
Felix Rilling <felix@f-rilling.com>
2018-03-24 09:30:45 +0100

    updated deps

--- test.js
+++ test.js
@@ -8,23 +8,23 @@ const {
 const fs = require("fs");
 
 // Normal API Access
-/* getTimeframes().then(data => console.log("Timeframes:", data)); */
+getTimeframes().then(data => console.log("Timeframes:", data));
 
-/* getFormats("2017-03")
+getFormats("2017-03")
     .then(data => console.log("Modes for timeframe 2017-03:", data))
-    .catch(console.error); */
+    .catch(console.error);
 
-/* getUsage("2017-03", "battlefactory-1760").then(data =>
+getUsage("2017-03", "battlefactory-1760").then(data =>
     console.log("Usage data for 2017-03 battlefactory-1760:", data)
-); */
+);
 
-/* getLeads("2017-03", "battlefactory-1760").then(data =>
+getLeads("2017-03", "battlefactory-1760").then(data =>
     console.log("Lead data for 2017-03 battlefactory-1760:", data)
-); */
+);
 
-/* getMovesets("2017-12", "gen7ou-0").then(data => {
+getMovesets("2017-12", "gen7ou-0").then(data => {
     console.log("Moveset data for 2017-12 gen7ou-0:", data);
-}); */
+});
 
 // Example: load latest OU data
 getTimeframes().then(timeframes => {
